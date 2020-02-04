60fe2d0a72728fd3b03dc37895f70e91ca14e3da
Dimas Nugroho <36952153+dznugroho@users.noreply.github.com>
2020-02-04 13:16:31 +0700

    update

--- application/models/M_Login.php
+++ application/models/M_Login.php
@@ -1,13 +1,13 @@
 <?php
 class M_Login extends CI_Model{
     function auth_admin($username,$password){
-        $query=$this->db->query("SELECT * FROM user WHERE username='$username' AND password=MD5('$password') LIMIT 1");
+        $query=$this->db->query("SELECT * FROM tb_user WHERE username='$username' AND password='$password' LIMIT 1");
         return $query;
     }
  
     //cek nim dan password mahasiswa
     function auth_pemimpin($username,$password){
-        $query=$this->db->query("SELECT * FROM user WHERE username='$username' AND password=MD5('$password') LIMIT 1");
+        $query=$this->db->query("SELECT * FROM tb_user WHERE username='$username' AND password='$password' LIMIT 1");
         return $query;
     }
  
