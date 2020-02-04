3d930c09c2b080be68bf4c9463d7dcfd5941127c
Indra <58221056+indradavidson@users.noreply.github.com>
2020-02-04 10:53:46 +0700

    edit

--- application/views/include/sidebar.php
+++ application/views/include/sidebar.php
@@ -1,10 +1,12 @@
 <div class="main-sidebar">
     <aside id="sidebar-wrapper">
-          <div class="sidebar-brand">
-            <a href="index.html">Stisla</a>
-          </div>
+    <div class="sidebar-brand ">
+				<a href="http://localhost/simpatif/index.php/welcome">
+				<img src="http://localhost/inventaris/assets/img/avatar/sidokufix.png" height="60" width="120" alt="">
+				</a>
+			</div>
           <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">St</a>
+            <a href="index.html">SD</a>
           </div>
           <ul class="sidebar-menu">
               <li class="menu-header">Dashboard</li>
