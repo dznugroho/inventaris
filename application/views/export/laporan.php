d069cc65b74ff4ce96ac0593ca8ef8e931dee671
Indra <58221056+indradavidson@users.noreply.github.com>
2020-02-23 14:01:11 +0700

    ,

---
+++ application/views/export/laporan.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title><?php echo SITE_NAME .": ". ucfirst($this->uri->segment(1)) ." - ". ucfirst($this->uri->segment(2)) ?></title>
+
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- CSS Libraries -->
+  <link rel="stylesheet" href="<?= base_url()?>node_modules/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?= base_url()?>node_modules/datatables.net-select-bs4/css/select.bootstrap4.min.css">  
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="<?= base_url()?>assets/css/style.css">
+  <link rel="stylesheet" href="<?= base_url()?>assets/css/components.css">
+</head>
+
+<body>
+  <div id="app">
+    <div class="main-wrapper">
+      <div class="navbar-bg"></div>
+
+        <?php $this->load->view('include/header.php')?>
+
+        <?php $this->load->view('include/sidebar.php')?>
+
+          <div class="main-content">
+        <section class="section">
+          <div class="section-header">
+            <h1>Laporan</h1>
+            <div class="section-header-breadcrumb">
+              <div class="breadcrumb-item active"><a href="<?php echo site_url('dashboard'); ?>">Dashboard</a></div>
+              <div class="breadcrumb-item">Laporan</div>
+            </div>
+          </div>
+          <div class="row"> 
+          <div class="form-group col-3">
+            <form action="<?php echo site_url('laporan/cari'); ?>" method=POST>
+              <select class="form-control" type="text" name="keyword" id="keyword">
+                <option value="">Pilih Nama Bidang</option>
+                <?php foreach($keyword as $row):?>
+              <option value="<?php echo $row->kode_bidang;?>"><?php echo $row->nama_bidang;?></option>
+              <?php endforeach;?>
+              </select>
+              </div> 
+              <div class="form-group col-3">
+                  <button class="btn btn-icon icon-left btn-primary" type="submit"><i class="fa fa-search"></i></button>
+                  <a href="<?php echo site_url('laporan'); ?>" class="btn btn-icon icon-left btn-danger" ><i class="fas fa-sync"></i> Reset</a>
+            </form>
+          </div>
+       </div>
+       <div class="row">
+            <div class="card-body">
+              <a target="_blank" class="btn btn-success" href="<?php echo base_url('laporan/print')?>"><i class="fa fa-print"></i> Print</a>
+            <div class="dropdown d-inline mr-2">
+               <div class="btn-group dropdown">
+                  <button type="button" class="btn btn-primary">
+                  <i class="fa fa-download"></i>
+                  </button>
+                      <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Export
+                      </button>
+                      <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 29px, 0px);">
+                        <a class="dropdown-item" href="<?php echo base_url('laporan/excel')?>">Excel</a>
+                      </div>
+                    </div>
+                  </div>
+          </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <div class="card-body">
+                    <div class="table-responsive">
+                      <table class="table table-striped" id="mytable">
+                        <thead>
+                          <tr>
+                            <th>No.</th>
+                            <th>Nama Bidang</th>
+                            <th>Nama sub bidang</th>
+                            <th>Nama kegiatan</th>
+                            <th>Waktu Mulai</th>
+                            <th>Waktu Selesai</th>
+                            <th>Anggaran</th>
+                            <th>Aksi</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                              $no = 0;
+                              foreach ($laporan->result() as $row):
+                                $no++;
+                            ?>
+                            <tr>
+                              <td><?php echo $no;?></td>
+                              <td><?php echo $row->nama_bidang;?></td>
+                              <td><?php echo $row->nama_sub;?></td>
+                              <td><?php echo $row->nama_kegiatan;?></td>
+                              <td><?php echo $row->waktu_mulai;?></td>
+                              <td><?php echo $row->waktu_selesai;?></td>
+                              <td><?php echo number_format($row->anggaran);?></td>
+                              
+                              <td>
+                              <a href="<?php echo site_url('riwayat_pilihan/detail_riwayat/'.$row->kode_usulan);?>" class="btn btn-sm btn-primary" >Detail</a>
+                              </td>
+                            </tr>
+                            <?php endforeach;?>
+                          </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </section>
+      </div>
+    
+        <?php $this->load->view('include/footer.php')?>
+
+</div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+<script src="<?= base_url()?>assets/js/stisla.js"></script>
+
+<!-- Template JS File -->
+<script src="<?= base_url()?>node_modules/datatables/media/js/jquery.dataTables.min.js"></script>
+  <script src="<?= base_url()?>node_modules/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <script src="<?= base_url()?>node_modules/datatables.net-select-bs4/js/select.bootstrap4.min.js"></script>
+
+  <!-- Template JS File -->
+  <script src="<?= base_url()?>assets/js/scripts.js"></script>
+  <script src="<?= base_url()?>assets/js/custom.js"></script>
+
+  <!-- Page Specific JS File -->
+  <script src="<?= base_url()?>assets/js/page/modules-datatables.js"></script>
+  <script type="text/javascript">
+		$(document).ready(function(){
+			$('#mytable').DataTable();
+		});
+	</script>
+</body>
+</html>
